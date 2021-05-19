44c0b923ad481a26cdcd386ad1f73ae99d3320d1
Patricia <patrimdm96@gmail.com>
2021-05-19 12:15:21 +0200

    code review interview

--- node-starter/src/index.js
+++ node-starter/src/index.js
@@ -20,9 +20,9 @@ const getCocktailsG = async () => {
 
 const logResults = async () => {
     const cocktails = await getCocktailsG()
-    const ingreds = Object.keys(cocktails[0]).filter(elm => elm.includes('strIngredient'))
+    const ingredients = Object.keys(cocktails[0]).filter(elm => elm.includes('strIngredient'))
     const measures = Object.keys(cocktails[0]).filter(elm => elm.includes('strMeasure'))
-    const fourIngr = cocktails.filter(elm => elm.strIngredient5 !== null)
+    const fourIngredientsCocktails = cocktails.filter(elm => elm.strIngredient5 !== null)
 
 
     console.log(`Cocktails beginning with G:
@@ -31,41 +31,56 @@ const logResults = async () => {
 
 
     console.log(`Cocktails with more than 4 ingredients:
-    ${fourIngr.map(elm => elm.strDrink)}`)
+    ${fourIngredientsCocktails.map(elm => elm.strDrink)}`)
     
 
     console.log(`Cocktails with just id/name/ingredients:`)
-    fourIngr.map(elm => {
-        const drinkIngs = ingreds.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
-        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${drinkIngs} ]`)
+    fourIngredientsCocktails.map(elm => {
+        const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
+        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${drinkIngredients} ]`)
     })
 
 
     console.log(`Cocktails with ingredient quantities:`)
-    fourIngr.map(elm => {
-        const drinkIngs = ingreds.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
-        const drinkMsr = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
-        const qtts = drinkIngs.map((e, i) => `{name: ${e}, quantity: ${drinkMsr[i]}}`)
-        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${qtts} ]`)
+    fourIngredientsCocktails.map(elm => {
+        const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
+        const drinkMeasures = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
+        const ingrQuantities = drinkIngredients.map((e, i) => `{name: ${e}, quantity: ${drinkMeasures[i]}}`)
+        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${ingrQuantities} ]`)
     })
 
+    // CAMBIAR MAP POR FOREACH!!
+    const printCocktails = (drinksArray, isAlcohol, printPrice) => {
+        drinksArray.filter(elm => isAlcohol ? elm.strAlcoholic === 'Alcoholic' : elm.strAlcoholic !== 'Alcoholic').map(elm => {
+            const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
+            const drinkMeasures = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
+            const ingrQuantities = drinkIngredients.map((e, i) => `{name: ${e}, quantity: ${drinkMeasures[i]}}`)
+            const cocktailPrice = (Math.random() * (10 - 5) + 5).toFixed(2)
+            console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ${printPrice ? `price: ${cocktailPrice},` : ''} ingredients: [ ${ingrQuantities} ]`)
+        })
+    }
 
     console.log(`Alcoholic / Non-alcoholic cocktails:`)
     console.log(`Alcoholic:`)
-    fourIngr.filter(elm => elm.strAlcoholic === 'Alcoholic').map(elm => {
-        const drinkIngs = ingreds.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
-        const drinkMsr = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
-        const qtts = drinkIngs.map((e, i) => `{name: ${e}, quantity: ${drinkMsr[i]}}`)
-        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${qtts} ]`)
-    })
+    printCocktails(fourIngredientsCocktails, true, false)
 
     console.log(`Non-Alcoholic:`)
-    fourIngr.filter(elm => elm.strAlcoholic !== 'Alcoholic').map(elm => {
-        const drinkIngs = ingreds.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
-        const drinkMsr = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
-        const qtts = drinkIngs.map((e, i) => `{name: ${e}, quantity: ${drinkMsr[i]}}`)
-        console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${qtts} ]`)
-    })
+    printCocktails(fourIngredientsCocktails, false, false)
+
+    //Precio random entre 5 y 10 y redondear a dos decimales
+    console.log(`Alcoholic / Non-alcoholic cocktails with price:`)
+    console.log(`Alcoholic:`)
+    printCocktails(fourIngredientsCocktails, true, true)
+
+    console.log(`Non-Alcoholic:`)
+    printCocktails(fourIngredientsCocktails, false, true)
+
+
+
+
+
+
+
 }
 
 
