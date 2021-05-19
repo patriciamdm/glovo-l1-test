1bc517766fda2b5d06453a6906019b334a1c8841
Patricia <patrimdm96@gmail.com>
2021-05-19 12:44:12 +0200

    interview code review fixes

--- node-starter/src/index.js
+++ node-starter/src/index.js
@@ -27,34 +27,34 @@ const logResults = async () => {
 
     console.log(`Cocktails beginning with G:
     Total: ${cocktails.length}
-    Names: ${cocktails.map(elm => elm.strDrink)}`)
+    Names: ${cocktails.forEach(elm => elm.strDrink)}`)
 
 
     console.log(`Cocktails with more than 4 ingredients:
-    ${fourIngredientsCocktails.map(elm => elm.strDrink)}`)
+    ${fourIngredientsCocktails.forEach(elm => elm.strDrink)}`)
     
 
     console.log(`Cocktails with just id/name/ingredients:`)
-    fourIngredientsCocktails.map(elm => {
+    fourIngredientsCocktails.forEach(elm => {
         const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
         console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${drinkIngredients} ]`)
     })
 
 
     console.log(`Cocktails with ingredient quantities:`)
-    fourIngredientsCocktails.map(elm => {
+    fourIngredientsCocktails.forEach(elm => {
         const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
         const drinkMeasures = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
         const ingrQuantities = drinkIngredients.map((e, i) => `{name: ${e}, quantity: ${drinkMeasures[i]}}`)
         console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ingredients: [ ${ingrQuantities} ]`)
     })
 
-    // CAMBIAR MAP POR FOREACH!!
     const printCocktails = (drinksArray, isAlcohol, printPrice) => {
-        drinksArray.filter(elm => isAlcohol ? elm.strAlcoholic === 'Alcoholic' : elm.strAlcoholic !== 'Alcoholic').map(elm => {
+        drinksArray.filter(elm => isAlcohol ? elm.strAlcoholic === 'Alcoholic' : elm.strAlcoholic !== 'Alcoholic').forEach(elm => {
             const drinkIngredients = ingredients.map(igr => elm[`${igr}`]).filter(elm => elm !== null)
             const drinkMeasures = measures.map(msr => elm[`${msr}`]).filter(elm => elm !== null)
             const ingrQuantities = drinkIngredients.map((e, i) => `{name: ${e}, quantity: ${drinkMeasures[i]}}`)
+            //Random price: between 5 and 10 and rounded with 2 decimals
             const cocktailPrice = (Math.random() * (10 - 5) + 5).toFixed(2)
             console.log(`name: ${elm.strDrink}, id: ${elm.idDrink}, ${printPrice ? `price: ${cocktailPrice},` : ''} ingredients: [ ${ingrQuantities} ]`)
         })
@@ -63,25 +63,15 @@ const logResults = async () => {
     console.log(`Alcoholic / Non-alcoholic cocktails:`)
     console.log(`Alcoholic:`)
     printCocktails(fourIngredientsCocktails, true, false)
-
     console.log(`Non-Alcoholic:`)
     printCocktails(fourIngredientsCocktails, false, false)
 
-    //Precio random entre 5 y 10 y redondear a dos decimales
     console.log(`Alcoholic / Non-alcoholic cocktails with price:`)
     console.log(`Alcoholic:`)
     printCocktails(fourIngredientsCocktails, true, true)
-
     console.log(`Non-Alcoholic:`)
     printCocktails(fourIngredientsCocktails, false, true)
 
-
-
-
-
-
-
 }
-
 
 logResults()
